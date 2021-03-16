a570bd9ef9ddf8bd4257d6a44d3fee0072cba793
luccavp12 <lucca12vp@gmail.com>
2021-03-16 02:41:38 -0700

    Implement code at the end of the day on Monday, set up for overall best accuracy implementation

--- main.py
+++ main.py
@@ -13,29 +13,35 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
 
     data2 = copy.deepcopy(data)
     current_set_of_features2 = copy.deepcopy(current_set_of_features)
+    for x in range(len(current_set_of_features2)):
+        # x = x + 1
+        current_set_of_features2[x] = current_set_of_features2[x] + 1
+        # print("current_set_of_features2[x]",current_set_of_features2[x])
+
     feature_to_add2 = copy.deepcopy(feature_to_add)
     current_set_of_features2.append(feature_to_add2)
-
+    
+    # print('')
+    # print("current_set_of_features2:", current_set_of_features2)
+    # print('')
+    # print("current_set_of_features2 [",end='')
+    # for x in current_set_of_features2:
+    #     x = x + 1
+        # print(x + 1, "", end='')
+    # print("]")
         
     for row in data2:
+        # print('len(row)', len(row))
         for val in range(1,len(row)):
             if val not in current_set_of_features2:
+                # print("val",val)
                 row[val] = 0
 
+    # for row in data2:
+    #     print(row)
+    #     break
 
-        #     print("val",val)
-        #     print("current_set_of_features2",current_set_of_features2)
-        #     # print("row[val]",row[val])
-        #     if val not in current_set_of_features2:
-        #         print(val,"is in current_set_of_features2!")
-        #         row[val] = 0
-        #         # print("val",val)
-        # # break
-
-    # np.set_printoptions(threshold=sys.maxsize)
     # print(data2)
-    # print(data2.to_string())
-
     size = len(data2)
     # print("size =", size)
     for i in range(size):
@@ -53,7 +59,7 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
             # print("Ask if", i+1, "is nearest neighbor with", j+1)
             
             if i != j:
-                distance = math.sqrt(sum((object_to_classify - data2[j][1:])**2))
+                distance = math.sqrt(sum((object_to_classify - data2[j][1:]) ** 2))
                 # print("distance", distance)
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
@@ -68,23 +74,25 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
         
     # print("number_correctly_classified", number_correctly_classified)
     accuracy = number_correctly_classified / size
-    print("accuracy:", accuracy)
     return accuracy
 
 
 def feature_search(data):
     current_set_of_features = []
+    current_set_of_features_features = []
+    overall_best = 0
+    best_accuracy_so_far_list = []
 
     for i in range(len(data[0]) - 1):
         print("On level", i + 1, "of the search tree")
 
-        print("current_set_of_features [",end='')
+        print("current_set_of_features == [",end='')
         for x in current_set_of_features:
             print(x + 1, "", end='')
         print("]")
         feature_to_add_at_this_level = []
         # current_set_of_features_ACC = leave_one_out_cross_validation(data, current_set_of_features, 20)
-        # print("current_set_of_features_ACC", current_set_of_features_ACC)
+        # print("current_set_of_features_ACC:", current_set_of_features_ACC)
         
         best_accuracy_so_far = 0
         for j in range(len(data[0]) - 1):
@@ -93,6 +101,7 @@ def feature_search(data):
                 # print("CALCULATING ACCURACY --------------------------------------------------------------------")
                 # print("current_set_of_features", current_set_of_features)
                 accuracy = leave_one_out_cross_validation(data, current_set_of_features, j+1)
+                print("accuracy:", accuracy)
                 if accuracy > best_accuracy_so_far:
                     best_accuracy_so_far = accuracy
                     feature_to_add_at_this_level = j
@@ -100,51 +109,21 @@ def feature_search(data):
         current_set_of_features.append(feature_to_add_at_this_level)
         print("On level", i+1, "I added feature", feature_to_add_at_this_level + 1, "to current set")
         print("best_accuracy_so_far", best_accuracy_so_far)
+        best_accuracy_so_far_list.append(best_accuracy_so_far)
+        # current_set_of_features_features.append(current_set_of_features)
         print("")
+    
+    print("current_set_of_features_features", current_set_of_features_features)
+    for x in range(len(best_accuracy_so_far_list)):
+        if best_accuracy_so_far_list[x] > overall_best:
+                    overall_best = best_accuracy_so_far_list[x]
+                    most_accurate_feature = x
 
-
+    print("highest accuracy was with:", current_set_of_features[0:most_accurate_feature], "with an accuracy of ", overall_best)
+ 
 if __name__ == '__main__':
     # data = pd.read_csv('CS170_SMALLtestdata__31.txt', delim_whitespace=True, header=None).values
     data = pd.read_csv('CS170_small_special_testdata__95.txt', delim_whitespace=True, header=None).values
-    # data = pd.read_csv('CS170_small_special_testdata__96.txt', delim_whitespace=True, header=None).values
     # print(data)
     # print(len(data[0]))
     feature_search(data) 
-
-
-
-
-
-# def feature_search(data):
-#     # csvFileList = []
-#     # for row in reader:
-#     #     csvFileList.append(row)
-        
-    
-#     # print("length of csvFileList:", len(csvFileList))
-#     # for row in csvFileList:
-#     #     for val in row:
-#     #         val = float(val)
-#             # print(type(val))
-
-#     # print(row)
-#     # print(csvFileList[0])
-
-#     current_set_of_features = []
-
-#     for i in range(len(data[0]) - 1):
-#         print("On level", i + 1, "of the search tree")
-#         feature_to_add_at_this_level = []
-#         best_accuracy_so_far = 0
-#         for j in range(len(data[0]) - 1):
-#             if j not in current_set_of_features: 
-#                 print("-- Considering adding feature", j + 1)
-#                 # print("CALCULATING ACCURACY --------------------------------------------------------------------")
-#                 accuracy = leave_one_out_cross_validation(data, current_set_of_features, j+1)
-#                 if accuracy > best_accuracy_so_far:
-#                     best_accuracy_so_far = accuracy
-#                     feature_to_add_at_this_level = j
-#         current_set_of_features.append(feature_to_add_at_this_level)
-#         print("On level", i+1, "I added feature", feature_to_add_at_this_level + 1, "to current set")
-
-#     # print(type(csvFileList[0][0]))
