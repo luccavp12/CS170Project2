79c0a10a1eab5f7b075e399c103f43d55adfa7c7
luccavp12 <lucca12vp@gmail.com>
2021-03-18 01:46:43 -0700

    Implement all required algorithms, stable version

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import math
 import pandas as pd
 import copy
 import sys
+import time
 
 def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add):
-    # accuracy = random.random()
-
     number_correctly_classified = 0
 
     data2 = copy.deepcopy(data)
@@ -18,8 +17,9 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
         current_set_of_features2[x] = current_set_of_features2[x] + 1
         # print("current_set_of_features2[x]",current_set_of_features2[x])
 
-    feature_to_add2 = copy.deepcopy(feature_to_add)
-    current_set_of_features2.append(feature_to_add2)
+    if feature_to_add != -1:
+        feature_to_add2 = copy.deepcopy(feature_to_add)
+        current_set_of_features2.append(feature_to_add2)
     
     # print('')
     # print("current_set_of_features2:", current_set_of_features2)
@@ -77,41 +77,60 @@ def leave_one_out_cross_validation(data, current_set_of_features, feature_to_add
     return accuracy
 
 
-def feature_search(data):
+###################################################################################################################
+###################################################################################################################
+
+
+def backward_elimination(data):
     current_set_of_features = []
     current_set_of_features_features = []
+    for x in range(len(data[0]) - 1):
+        current_set_of_features.append(x)
+
+    # print("current_set_of_features", current_set_of_features)
+    print("In the beginning, with all features, the accuracy of backward elimination is:", leave_one_out_cross_validation(data,current_set_of_features,-1))
+
     overall_best = 0
     best_accuracy_so_far_list = []
 
     for i in range(len(data[0]) - 1):
-        print("On level", i + 1, "of the search tree")
+        # print("On level", i + 1, "of the search tree")
 
-        print("current_set_of_features == [",end='')
-        for x in current_set_of_features:
-            print(x + 1, "", end='')
-        print("]")
-        feature_to_add_at_this_level = []
+        # print("current_set_of_features == [",end='')
+        # for x in current_set_of_features:
+        #     print(x + 1, "", end='')
+        # print("]")
+
+        feature_to_remove_at_this_level = []
         # current_set_of_features_ACC = leave_one_out_cross_validation(data, current_set_of_features, 20)
         # print("current_set_of_features_ACC:", current_set_of_features_ACC)
         
         best_accuracy_so_far = 0
         for j in range(len(data[0]) - 1):
-            if j not in current_set_of_features: 
-                print("-- Considering adding feature", j+1)
+            if j in current_set_of_features: 
+                # print("-- Considering removing feature", j+1)
                 # print("CALCULATING ACCURACY --------------------------------------------------------------------")
                 # print("current_set_of_features", current_set_of_features)
-                accuracy = leave_one_out_cross_validation(data, current_set_of_features, j+1)
-                print("accuracy:", accuracy)
+                removed_current_set_of_features = copy.deepcopy(current_set_of_features)
+                removed_current_set_of_features.remove(j)
+                accuracy = leave_one_out_cross_validation(data, removed_current_set_of_features, -1)
+                # print("accuracy:", accuracy)
                 if accuracy > best_accuracy_so_far:
                     best_accuracy_so_far = accuracy
-                    feature_to_add_at_this_level = j
+                    feature_to_remove_at_this_level = j
 
-        current_set_of_features.append(feature_to_add_at_this_level)
-        print("On level", i+1, "I added feature", feature_to_add_at_this_level + 1, "to current set")
+        current_set_of_features_copy = copy.deepcopy(current_set_of_features)
+        current_set_of_features_features.append(current_set_of_features_copy)
+        current_set_of_features.remove(feature_to_remove_at_this_level)
+        print("On level", i+1, "I removed feature", feature_to_remove_at_this_level + 1, "from current set")
+        print("current_set_of_features == [",end='')
+        for x in current_set_of_features:
+            print(x + 1, "", end='')
+        print("]")
         print("best_accuracy_so_far", best_accuracy_so_far)
         best_accuracy_so_far_list.append(best_accuracy_so_far)
         # current_set_of_features_features.append(current_set_of_features)
-        print("")
+        # print("")
     
     # print("current_set_of_features_features", current_set_of_features_features)
     for x in range(len(best_accuracy_so_far_list)):
@@ -121,14 +140,114 @@ def feature_search(data):
 
     # print("current set of features:", current_set_of_features)
     # print("most accurate feature:", most_accurate_feature)
-    print("highest accuracy was with: [ ", end='')
-    for item in current_set_of_features[:most_accurate_feature + 1]:
-        print(item,end=' ')
-    print("] with an accuracy of ", overall_best)
+    most_accurate_features_display = [x + 1 for x in current_set_of_features_features[most_accurate_feature + 1]]
+    print("highest accuracy with backward elimination was:", most_accurate_features_display, "with an accuracy of", overall_best)
+    # print("] with an accuracy of ", overall_best)
+    # print("highest accuracy was with: [ ", end='')
+    # for item in current_set_of_features[:most_accurate_feature + 1]:
+    #     print(item,end=' ')
+    # print("] with an accuracy of ", overall_best)
+
+###################################################################################################################
+###################################################################################################################
+
+def forward_selection(data):
+    current_set_of_features = []
+    current_set_of_features_features = []
+    overall_best = 0
+    best_accuracy_so_far_list = []
+
+    print("In the beginning, with no features, the accuracy of forward selection is:", leave_one_out_cross_validation(data,[],-1))
+
+    for i in range(len(data[0]) - 1):
+        # print("On level", i + 1, "of the search tree")
+
+        # print("current_set_of_features == [",end='')
+        # for x in current_set_of_features:
+        #     print(x + 1, "", end='')
+        # print("]")
+        feature_to_add_at_this_level = []
+        # current_set_of_features_ACC = leave_one_out_cross_validation(data, current_set_of_features, 20)
+        # print("current_set_of_features_ACC:", current_set_of_features_ACC)
+        
+        best_accuracy_so_far = 0
+        for j in range(len(data[0]) - 1):
+            if j not in current_set_of_features: 
+                # print("-- Considering adding feature", j+1)
+                # print("CALCULATING ACCURACY --------------------------------------------------------------------")
+                # print("current_set_of_features", current_set_of_features)
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, j+1)
+                # print("accuracy:", accuracy)
+                if accuracy > best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_add_at_this_level = j
+
+        current_set_of_features_copy = copy.deepcopy(current_set_of_features)
+        current_set_of_features_features.append(current_set_of_features_copy)
+        current_set_of_features.append(feature_to_add_at_this_level)
+        print("On level", i+1, "I added feature", feature_to_add_at_this_level + 1, "to current set")
+        print("current_set_of_features == [",end='')
+        for x in current_set_of_features:
+            print(x + 1, "", end='')
+        print("]")
+        print("best_accuracy_so_far", best_accuracy_so_far)
+        best_accuracy_so_far_list.append(best_accuracy_so_far)
+        # current_set_of_features_features.append(current_set_of_features)
+        # print("")
+    
+    # print("current_set_of_features_features", current_set_of_features_features)
+    for x in range(len(best_accuracy_so_far_list)):
+        if best_accuracy_so_far_list[x] > overall_best:
+                    overall_best = best_accuracy_so_far_list[x]
+                    most_accurate_feature = x
+
+    # print("current set of features:", current_set_of_features)
+    # print("most accurate feature:", most_accurate_feature)
+    # print("highest accuracy was with: [ ", end='')
+    # for item in current_set_of_features[:most_accurate_feature + 1]:
+    #     print(item,end=' ')
+    # print("] with an accuracy of ", overall_best)
+    most_accurate_features_display = [x + 1 for x in current_set_of_features_features[most_accurate_feature + 1]]
+    print("highest accuracy on forward selection was with:", most_accurate_features_display, "with an accuracy of", overall_best)
+
  
 if __name__ == '__main__':
-    # data = pd.read_csv('CS170_SMALLtestdata__31.txt', delim_whitespace=True, header=None).values
-    data = pd.read_csv('CS170_small_special_testdata__95.txt', delim_whitespace=True, header=None).values
+    # data = pd.read_csv('CS170_small_special_testdata__95.txt', delim_whitespace=True, header=None).values
     # print(data)
     # print(len(data[0]))
-    feature_search(data) 
+    # t0 = 0
+    # data = []
+
+    fileChoice = input("(1) Small Data set\n(2) Large Data set(takes a while)\n(Press 1 or 2)\n")
+    searchAlgoChoice = input("(1) Forward Selection\n(2) Backward Elimination\n(Press 1 or 2)\n")
+
+    if fileChoice == '1' and searchAlgoChoice == '1':
+        data = pd.read_csv('CS170_SMALLtestdata__31.txt', delim_whitespace=True, header=None).values 
+        t0 = time.time() 
+        forward_selection(data)
+        t1 = time.time()
+        totalTime = t1-t0
+        print("totalTime:", totalTime, "seconds")
+    elif fileChoice == '2' and searchAlgoChoice == '1':
+        data = pd.read_csv('CS170_largetestdata__70.txt', delim_whitespace=True, header=None).values
+        t0 = time.time() 
+        forward_selection(data)
+        t1 = time.time()
+        totalTime = t1-t0
+        print("totalTime:", totalTime, "seconds")
+    elif fileChoice == '1' and searchAlgoChoice == '2':
+        data = pd.read_csv('CS170_SMALLtestdata__31.txt', delim_whitespace=True, header=None).values 
+        t0 = time.time()
+        backward_elimination(data)
+        t1 = time.time()
+        totalTime = t1-t0
+        print("totalTime:", totalTime, "seconds")
+    elif fileChoice == '2' and searchAlgoChoice == '2':
+        data = pd.read_csv('CS170_largetestdata__70.txt', delim_whitespace=True, header=None).values
+        t0 = time.time()
+        backward_elimination(data)
+        t1 = time.time()
+        totalTime = t1-t0
+        print("totalTime:", totalTime, "seconds")
+        
+    
